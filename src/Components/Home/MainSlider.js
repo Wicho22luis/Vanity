ab6d5abf2315961e81d5213066f3c7d8ffe73b76
Luis Jasso <ti@ferremates.com>
2023-01-26 13:14:51 -0600

    Prueba 3

--- src/Components/Home/MainSlider.js
+++ src/Components/Home/MainSlider.js
@@ -9,7 +9,7 @@ function Slider() {
             <Carousel.Item>
                 <img className="d-block w-100 imageSlider1" src={Banner} alt="First slide"/>
                 <Carousel.Caption className='captionContainer'>
-                    <h1 className='textoSlider'>TE AMO MI NIÑA</h1>
+                    <h1 className='textoSlider'>Sortija en ORO</h1>
                     <h4 className='textoSlider'>con incrustaciones de diamantes</h4>
                     <Button variant="light" className='textoSlider btnSlider'>Conoce más aquí</Button>
                 </Carousel.Caption>
@@ -18,7 +18,7 @@ function Slider() {
                 <img className="d-block w-100 imageSlider1" src={Banner}alt="Second slide" />
 
                 <Carousel.Caption>
-                    <h1 className='textoSlider'>CASATE CONMIGO</h1>
+                    <h1 className='textoSlider'>Sortija en ORO</h1>
                     <h4 className='textoSlider'>con incrustaciones de diamantes</h4>
                     <Button variant="light" className='textoSlider'>Conoce más aquí</Button>
                 </Carousel.Caption>
